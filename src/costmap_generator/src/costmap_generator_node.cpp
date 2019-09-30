649dd468bc2dd4c8d16e6ba25f51854dd7a7d850
abhirajtiwari <abhirajtiwari@gmail.com>
2019-09-30 20:12:39 +0530

    something working

--- src/costmap_generator/src/costmap_generator_node.cpp
+++ src/costmap_generator/src/costmap_generator_node.cpp
@@ -13,13 +13,14 @@
 
 #include <pcl/point_types.h>
 
-#define MAP_WIDTH 30
-#define MAP_HEIGHT 30
+#define MAP_WIDTH 61
+#define MAP_HEIGHT 61
 #define MAP_RESOLUTION 0.5
 
 ros::Publisher occu_grid_pub;
 geometry_msgs::Pose current_pose;
 geometry_msgs::Pose fake_pose;
+nav_msgs::OccupancyGrid grid;
 
 void getPose (const nav_msgs::Odometry::ConstPtr& data) {
 	current_pose = data->pose.pose;
@@ -36,29 +37,41 @@ void addObstacles (const sensor_msgs::PointCloud2::ConstPtr& icloud) {
 	fake_pose.orientation.z = 0;
 	fake_pose.orientation.w = 0;
 
-	nav_msgs::OccupancyGrid grid;
-	grid.header.frame_id = "temp_map";
 	grid.header.stamp = ros::Time::now();
 	grid.info.resolution = MAP_RESOLUTION;
-	grid.info.width = MAP_WIDTH;
-	grid.info.height = MAP_HEIGHT;
+	grid.info.width = (MAP_WIDTH*100)/(MAP_RESOLUTION*100);
+	grid.info.height = (MAP_HEIGHT*100)/(MAP_RESOLUTION*100);
 	/* grid.info.origin = current_pose; */
 	grid.info.origin = fake_pose;
 
-	//Convert Point Cloud so that it is useable CONTINUE FROM HERE
+	//Convert Point Cloud so that it is useable 
 	pcl::PCLPointCloud2 *cloud = new pcl::PCLPointCloud2;
 	pcl_conversions::toPCL(*icloud, *cloud);
 	pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud(new pcl::PointCloud<pcl::PointXYZ>); //cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromPCLPointCloud2(*cloud,*input_cloud);	
 
 	//make z = 0
-	for (auto &p : input_cloud->points) p.z = 0; 
+	std::vector<signed char> data;
+	int cm_size = ((MAP_WIDTH*100)/(MAP_RESOLUTION*100))*((MAP_HEIGHT*100)/(MAP_RESOLUTION*100));
+	data.resize(cm_size,0); //Make this -1 when adding plane
+	for (auto p : input_cloud->points) {
+		int y_t = ((MAP_HEIGHT*100)/2 - (int)p.z*100)/(MAP_RESOLUTION*100);
+		int x_t = ((MAP_HEIGHT*100)/2 + (int)p.x*100)/(MAP_RESOLUTION*100);
+		data[y_t*((MAP_WIDTH*100)/(MAP_RESOLUTION*100)) + x_t] += 10;
+	}
+	/* int cm_min = *std::min_element(data.begin(), data.end()); */
+	/* int cm_max = *std::max_element(data.begin(), data.end()); */
+	/* for (auto &p : data) */ 
+	/* 	p = (int) 100*(p-cm_min)/(cm_max-cm_min); */
+	grid.data = data;	
+	occu_grid_pub.publish(grid);
 }
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "costmap_generator");
 	ros::NodeHandle nh;
 	occu_grid_pub = nh.advertise<nav_msgs::OccupancyGrid>("ocgrid", 10);
+	/* ros::Subscriber pl_sub = nh.subscribe("plane", 10, addPlane); */
 	ros::Subscriber ob_sub = nh.subscribe("obstacle", 10, addObstacles);
 	/* ros::Subscriber pose_sub = nh.subscribe("odom", 50, getPose); */
 	ros::spin();
