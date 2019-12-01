5c5c1131fb991f86186f85d6ca3654f9203f4b56
abhirajtiwari <abhirajtiwari@gmail.com>
2019-12-01 16:58:15 +0530

    wait for sync

--- src/obstacle_avoidance_2/scripts/mask.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def mask(in_arr, thetas, func):
-    return func(in_arr, thetas)
---
+++ src/obstacle_avoidance_2/scripts/masks.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def cardiod(a, thetas):
+    return a*(1+np.cos(thetas))
