91cb5613d1df73b3bc32efaf4408e7a2ba5dff5e
armaan10 <armaan.ashfaque@gmail.com>
2019-12-01 18:50:25 +0530

    8m as ideal cardiod

--- src/obstacle_avoidance_2/scripts/checkclear.py
+++ src/obstacle_avoidance_2/scripts/checkclear.py
@@ -3,16 +3,19 @@ import numpy as np
 from sensor_msgs.msgs import Imu
 from tf.transformations import euler_from_quaternion
 #needs to ne tuned
-clear_thresh=2
+#-2.39982771873
+
+
 def diff(op1,op2,start,end):
 	sin_comps= op1*sines[start:end]-op2*sines[start:end]
 	cos_comps=op1*coses[start:end]-op2*coses[start:end]
 	mag=(np.sum(sin_comps)**2+np.sum(cos_comps))**0.5
 	return mag
 
-def check_clear(cardiod):
+def check_clear(cardiod,ori_card,clear_thresh):
 	#47.5 degrees on both sides 
 	arr_len=len(cardiod)
+	cardiod[cardiod>8]=8
 	limit_ind=int(47.5*arr_len/275)
 	b1_cheek=cardiod[0:limit_ind]
 	b2_cheek=cardiod[arr_len-limit_ind:arr_len]
@@ -22,3 +25,4 @@ def check_clear(cardiod):
 	if(d1<clear_thresh and d2<clear_thresh):
 		return True
 	return False
+def force_turn()
