35ba4e206415163bc70debb27363e395a01ae4ac
abhirajtiwari <abhirajtiwari@gmail.com>
2019-12-01 15:08:35 +0530

    renamed to masks

--- src/obstacle_avoidance_2/scripts/cardiod.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def cardiod(a, thetas):
-    return a*(1-np.cos(thetas + (np.pi/2)))
---
+++ src/obstacle_avoidance_2/scripts/mask.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def mask(in_arr, thetas, func):
+    return func(in_arr, thetas)
