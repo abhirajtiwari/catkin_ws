02eed88db36a35c7f4251fe6f5d90a1355de0087
abhirajtiwari <abhirajtiwari@gmail.com>
2020-01-15 22:09:06 +0530

    new motor code

--- src/obstacle_avoidance_2/scripts/decoder.py
+++ src/obstacle_avoidance_2/scripts/decoder.py
@@ -21,42 +21,39 @@ def joystick_decoder():
     global gear,x_joy,y_joy
     while(True):
 
-        hill_assist=False
-        gear_pack = 0b00000001
-        if not hill_assist:
+        # hill_assist=False
+        # gear_pack = 0b00000001
+        # if not hill_assist:
 
-            gear_pack = (0b00001111 & gear)
-        elif hill_assist:
-            gear_pack = (0b00001111 & gear)
-            gear_pack |= 0b00010000
+        #     gear_pack = (0b00001111 & gear)
+        # elif hill_assist:
+        #     gear_pack = (0b00001111 & gear)
+        #     gear_pack |= 0b00010000
 
-        x1 = 0b00001111 & (x_joy >> 10)
-        x1 |= 0b00100000
+        # x1 = 0b00001111 & (x_joy >> 10)
+        # x1 |= 0b00100000
 
-        x2 = 0b000011111 & (x_joy >> 5)
-        x2 |= 0b01000000
+        # x2 = 0b000011111 & (x_joy >> 5)
+        # x2 |= 0b01000000
 
-        x3 = 0b00000000011111 & (x_joy >> 0)
-        x3 |= 0b01100000
+        # x3 = 0b00000000011111 & (x_joy >> 0)
+        # x3 |= 0b01100000
 
-        y1 = 0b00001111 & (y_joy >> 10)
-        y1 |= 0b10000000
+        # y1 = 0b00001111 & (y_joy >> 10)
+        # y1 |= 0b10000000
 
-        y2 = 0b000011111 & (y_joy >> 5)
-        y2 |= 0b10100000
+        # y2 = 0b000011111 & (y_joy >> 5)
+        # y2 |= 0b10100000
 
-        y3 = 0b00000000011111 & (y_joy >> 0)
-        y3 |= 0b11000000
+        # y3 = 0b00000000011111 & (y_joy >> 0)
+        # y3 |= 0b11000000
 
-        
         ser.write('m'.encode())
-        ser.write(bytes([gear_pack]))
-        ser.write(bytes([x1]))
-        ser.write(bytes([x2]))
-        ser.write(bytes([x3]))
-        ser.write(bytes([y1]))
-        ser.write(bytes([y2]))
-        ser.write(bytes([y3]))
+        ser.write(gear)
+        ser.write('s'.encode())
+        ser.write(str(x_joy).encode().zfill(5))
+        ser.write('f'.encode())
+        ser.write(str(y_joy).encode().zfill(5))
         ser.write('n'.encode())
             
 if __name__ == '__main__':
