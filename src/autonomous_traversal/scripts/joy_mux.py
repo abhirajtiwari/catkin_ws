06f4c10236b91947cf2ca384180ec54c48ff592c
abhirajtiwari <abhirajtiwari@gmail.com>
2020-01-21 19:23:29 +0530

    incomplete mux

--- src/autonomous_traversal/scripts/joy_mux.py
+++ src/autonomous_traversal/scripts/joy_mux.py
@@ -1,6 +1,54 @@
 #!/usr/bin/python3
 import rospy
 import numpy as np
+import math
+import time
 
-from sensor_msgs.msg import LaserScan
+import serial
+
+from std_msgs.msg import String
+
+from gps_traversal.py import GPSTraversal
+
+class JoyMux:
+    '''
+    Let all the data be numpy arrays converted to string having 3 fields
+    priority | r | theta
+    priority be just an extensible feature to be added later could be used for overriding
+    '''
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyUSB', 115200)
+        self.rs_data = None
+        self.sick_data = None
+        self.gps_data = None
+
+        self.rs_sub = rospy.Subscriber('', String, queue_size = 10)
+        self.sick_sub = rospy.Subscriber('sick_cmd', String, queue_size = 10)
+        self.gps_ob = GPSTraversal()
+
+        self.start()
+
+    def rs_callback(self, data):
+        self.rs_data = np.fromstring(data.data)
+
+    def sick_callback(self, data):
+        self.gps_data = np.fromstring(data.data)
+
+    def start(self):
+        
+        #destroy turn values within 5degs
+        self.rs_data[2] = 0 if (abs(self.rs_data[2]) <= 5) else self.rs_data[2]
+        self.sick_data[2] = 0 if (abs(self.sick_data[2]) <= 5) else self.sick_data[2]
+
+        #just before serial write fill these
+        rospy.logdebug()
+
+
+    def ellipticalDisktoSquare(self, x, y):
+    
+
+if __name__ == '__main__':
+    rospy.init_node('joy_mux')
+    JoyMux()
+    rospy.spin()
 
